db7953b612cba8e0cc99303683c52e5267eab720
sumukhj1219 <sumukhjoshi75@gmail.com>
2024-09-14 11:45:05 +0530

    modify: added image-optimize route

--- app/(app)/(optimizations)/image-optimize/[imageId]/page.tsx
+++ app/(app)/(optimizations)/image-optimize/[imageId]/page.tsx
@@ -1,9 +1,29 @@
+'use server'
+import prisma from '@/utils/db'
+import Image from 'next/image'
 import React from 'react'
 
-const ImageOptimize = () => {
+interface ImageOptimizeProps{
+    params:{
+        imageId:string
+    }
+}
+
+const ImageOptimize = async({params}:ImageOptimizeProps) => {
+  const image = await prisma.images.findUnique({
+    where:{
+        id:params.imageId
+    }
+  })
   return (
-    <div>
-      Optimize Your Image
+    <div className='mt-14 max-w-3xl'>
+      <Image 
+      src={image?.url}
+      width={image?.width}
+      height={image?.height}
+      alt={image?.name}
+      
+      />
     </div>
   )
 }
